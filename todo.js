6a3d245004e2f1242b0d931ded69560c6d331730
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-26 05:30:20 +0530

    add, display and delete data added

--- todo.js
+++ todo.js
@@ -1,10 +1,30 @@
-console.log('Hello World!');
+let todoList = [];
+displayItems();
 
 function addItem(){
-    let inputElement = document.querySelector('#todo-input').value;
-    let inputDate = document.querySelector('#todo-date').value;
-    console.log(inputDate, inputElement)
-    console.log();
-    document.querySelector('#todo-input').value = '';
-    document.querySelector('#todo-date').value = '';
+    let inputElement = document.querySelector('#todo-input');
+    let inputDate = document.querySelector('#todo-date');
+    let itemValue = inputElement.value;
+    let dueDate = inputDate.value;
+    todoList.push({item: itemValue, duedate: dueDate})
+    console.log(itemValue, dueDate);
+    inputElement.value = '';
+    inputDate.value = '';
+    
+    displayItems()
 }
+
+function displayItems() {
+    let toDoShow = document.querySelector('.containerElement')
+    let newHtml = '';
+    for (let i = 0; i < todoList.length; i++) {
+        let  {item, duedate} = todoList[i]
+        newHtml += `
+            <span>${item}</span>
+                <span>${duedate}</span>
+                <button id="btn-delete" onclick="todoList.splice(${i}, 1);
+                displayItems();">Delete</button>
+          `;
+    }
+    toDoShow.innerHTML = newHtml;
+  }
